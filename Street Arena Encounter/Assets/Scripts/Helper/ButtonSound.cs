bf88d622f6e876a1fa7d9064a2635ee2bd5c6ed2
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-03 23:12:07 +0100

    Added ButtonSound Class to play sounds in the menu;

---
+++ Street Arena Encounter/Assets/Scripts/Helper/ButtonSound.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+
+public class ButtonSound : MonoBehaviour
+{
+    GameObject m_currentGObj;
+    InputSystemUIInputModule m_isuim;
+
+    void Start()
+    {
+        m_isuim = GetComponent<InputSystemUIInputModule>();
+    }
+
+    void Update()
+    {
+        PlayMoveSound();
+        PlaySelectSound();
+    }
+
+    void PlayMoveSound()
+    {
+        GameObject gobj = EventSystem.current.currentSelectedGameObject;
+        if (gobj == null)
+            return;
+
+        if (gobj != m_currentGObj)
+            AudioManager.Instance.Play(AudioManager.Instance.m_ButtonMove);
+        m_currentGObj = gobj;
+    }
+
+    void PlaySelectSound()
+    {
+        if (m_isuim.submit.action.triggered)
+            AudioManager.Instance.Play(AudioManager.Instance.m_ButtonSelect);
+    }
+}
