6bb7f6bc2ccfa842be4e6d444600500c8a2edc4d
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-06 17:58:48 +0100

    Cleaned up messy code of movement controller; Added IsGrounded Check; To-Do, Height according to crouch origin up;

--- Street Arena Encounter/Assets/Scripts/Gameplay/MovementController.cs
+++ Street Arena Encounter/Assets/Scripts/Gameplay/MovementController.cs
@@ -29,57 +29,56 @@ public class MovementController : MonoBehaviour
     {
         //InputValues
         desiredDirection.x = m_PlayerInfo.Input.m_controls.m;
-
-        //Chrouch Movement
-        if (desiredDirection.x * m_PlayerInfo.Forward < 0 && m_PlayerInfo.Input.m_controls.c)
-        {
-            m_PlayerInfo.Ani.SetFloat("Move", 0);
-            m_PlayerInfo.Ani.SetBool("Crouch", true);
-            m_PlayerInfo.Char.height = m_PlayerInfo.GP.CrouchHeight;
-            Fall();
-            return;
-        }
-
-        //Jumping
-        m_PlayerInfo.Ani.SetBool("Jump", false);
-        if (m_PlayerInfo.Char.isGrounded && !m_PlayerInfo.Input.m_controls.c)
-        {
-            if (m_PlayerInfo.Input.m_controls.j)
-            {
-                m_PlayerInfo.Ani.SetBool("Jump", true);
-                desiredDirection.y = m_PlayerInfo.GP.JumpForce;
-                //Jumping Force forward or backward
-                m_force = desiredDirection.x * m_PlayerInfo.GP.JumpDashForce;
-            }
-            if (m_PlayerInfo.Input.m_controls.d)
-                //Dashing forward or backward
-                m_force = desiredDirection.x * m_PlayerInfo.GP.DashForce;
-        }
-
         //SetAnimator Pramater for Movement
         m_PlayerInfo.Ani.SetFloat("Move", m_PlayerInfo.Input.m_controls.m * m_PlayerInfo.Forward);
 
-        //SetAnimator Pramater for Crouching
-        m_PlayerInfo.Ani.SetBool("Crouch", m_PlayerInfo.Input.m_controls.c);
-        //Crouching
-        m_PlayerInfo.Char.height = m_PlayerInfo.Ani.GetBool("Crouch") ? m_PlayerInfo.GP.CrouchHeight : m_PlayerInfo.GP.PlayerHeight;
+        //Chrouch Movement
+        if (desiredDirection.x * m_PlayerInfo.Forward >= 0)
+        {
+            //SetAnimator Pramater for Crouching
+            m_PlayerInfo.Ani.SetBool("Crouch", m_PlayerInfo.Input.m_controls.c);
+        }
+        else if (m_PlayerInfo.Input.m_controls.c)
+        {
+            desiredDirection.x = 0;
+            m_PlayerInfo.Ani.SetFloat("Move", 0);
+            m_PlayerInfo.Ani.SetBool("Crouch", m_PlayerInfo.Input.m_controls.c);
+            return;
+        }
+        else
+        {
+            m_PlayerInfo.Ani.SetBool("Crouch", m_PlayerInfo.Input.m_controls.c);
+        }
+
+        //SetAnimator Pramater for Jumping
+        m_PlayerInfo.Ani.SetBool("Jump", false);
+        //Jumping
+        if (IsGrounded() && !m_PlayerInfo.Input.m_controls.c)
+        {
+            if (m_PlayerInfo.Input.m_controls.j)
+            {
+                //SetAnimator Pramater for Jumping
+                m_PlayerInfo.Ani.SetBool("Jump", true);
+                //Set InputValues for Jumping
+                desiredDirection.y = m_PlayerInfo.GP.JumpForce;
+                //Jumping Dashing forward/ backward
+                AttackManager.Instance.Dash(m_PlayerInfo, m_PlayerInfo.GP.JumpDashDistance * desiredDirection.x, 0.25f);
+            }
+            if (m_PlayerInfo.Input.m_controls.d)
+            {
+                //Dashing forward/ backward
+                AttackManager.Instance.Dash(m_PlayerInfo, m_PlayerInfo.GP.DashDistance * desiredDirection.x, 0.25f);
+            }
+        }
 
         //Calculating Gravity
+        if (!IsGrounded())
         desiredDirection.y += m_PlayerInfo.GP.GravityForce * Time.deltaTime;
-        desiredDirection.y = Mathf.Clamp(desiredDirection.y, m_PlayerInfo.GP.GravityForce, 10);
-        //Calculating Force for Dashing
-        desiredDirection.x += m_force;
-        m_force -= m_force * 3 * Time.deltaTime;
+            //desiredDirection.y = Mathf.Max(desiredDirection.y, -25);
 
 
         //hand over desiredDirection to charController;
         m_PlayerInfo.Char.Move(desiredDirection * Time.deltaTime);
-
-        //Lock target in movable space
-        Constraint();
-
-        //Set State of current Movement
-        SetState();
     }
     /// <summary>
     /// When the Contraint is true then perform Fall() instead of Move()
@@ -92,40 +91,15 @@ public class MovementController : MonoBehaviour
 
         //Calculating Gravity
         desiredDirection.y += m_PlayerInfo.GP.GravityForce * Time.deltaTime;
-        desiredDirection.y = Mathf.Clamp(desiredDirection.y, m_PlayerInfo.GP.GravityForce, 10);
 
         //hand over desiredDirection to charController;
         m_PlayerInfo.Char.Move(desiredDirection * Time.deltaTime);
 
-        m_PlayerInfo.Char.height = m_PlayerInfo.GP.PlayerHeight;
-
-
-        SetState();
+        //m_PlayerInfo.Char.height = m_PlayerInfo.GP.PlayerHeight;
     }
 
     #region -Utilities
-    /// <summary>
-    /// Calculates the distance of the player and returns a bool when a threshold is stepped over for Max Distance between the two players
-    /// </summary>
-    void Constraint()
-    {
-        float xPos = 0;
-        float offSet = m_PlayerInfo.GP.MinDistance * 0.5f;
 
-        if (m_PlayerInfo.IsLeft)
-            xPos = Mathf.Clamp(
-                transform.localPosition.x, 
-                -9 + offSet, 
-                GameManager.Instance.m_Player_R.Player.transform.localPosition.x - offSet);
-        else
-            xPos = Mathf.Clamp(
-                transform.localPosition.x, 
-                GameManager.Instance.m_Player_L.Player.transform.localPosition.x + offSet, 
-                9 - offSet);
-
-        Vector3 newPos = new Vector3(xPos, transform.localPosition.y, 0);
-        transform.localPosition = newPos;
-    }
     /// <summary>
     /// Changes the Flag Enum representing the state the player is currently in
     /// </summary>
@@ -151,6 +125,12 @@ public class MovementController : MonoBehaviour
         else
             m_CurrentState &= ~EMovementStates.Jump;
     }
+    public void SetHeight()
+    {
+        //Crouching
+        m_PlayerInfo.Char.height = m_PlayerInfo.Ani.GetBool("Crouch") ? m_PlayerInfo.GP.CrouchHeight : m_PlayerInfo.GP.PlayerHeight;
+        m_PlayerInfo.Char.radius = m_PlayerInfo.GP.MinDistance;
+    }
     /// <summary>
     /// Resets the Values of the Players Height and Animation
     /// </summary>
@@ -162,6 +142,13 @@ public class MovementController : MonoBehaviour
 
         m_PlayerInfo.Ani.SetFloat("Move", 0);
         m_PlayerInfo.Ani.SetBool("Crouch", false);
+    }
+    public bool IsGrounded()
+    {
+        return GameManager.Instance.BoolRayCast(
+            transform.localPosition + Vector3.up * 0.005f, 
+            Vector3.down, 
+            0.005f);
     }
     #endregion
 }
