8ec61a7b71831ef447dcf19ba7d8ccd5203f55b5
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-07 19:15:27 +0100

    Adjusted order of field in gp_settings;

--- Street Arena Encounter/Assets/Scripts/Scriptables/Scripts/GP_Settings.cs
+++ Street Arena Encounter/Assets/Scripts/Scriptables/Scripts/GP_Settings.cs
@@ -4,14 +4,14 @@
 public class GP_Settings : ScriptableObject
 {
     public float PlayerRadius = 2;
+    public float PlayerHeight = 3.1f;
     public float MaxPlayerDistance = 25;
+    public float MovementSpeed = 1;
+    public float GravityForce = -40;
     public float JumpForce = 10;
     public float JumpDashForce = 15;
     public float DashForce = 30;
-    public float GravityForce = -40;
-    public float MovementSpeed = 1;
-    public float PlayerHeight = 3.1f;
-    public float PlayerStartPos = 3;
     [Range(0.1f, 1)] public float CrouchHeight = 0.74f;
     public float Health = 100;
+    public float PlayerStartPos = 3;
 }
