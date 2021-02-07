81963cbfca95038192f2bb9db888dd4f701d2b0c
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-07 19:23:08 +0100

    Improved and finished movementController; To-Do: fix setHight, work on Dashing backwards and sprinting forward till collision, then drop enemy;

--- Street Arena Encounter/Assets/Scripts/Gameplay/MovementController.cs
+++ Street Arena Encounter/Assets/Scripts/Gameplay/MovementController.cs
@@ -18,16 +18,24 @@ public class MovementController : MonoBehaviour
     [HideInInspector] public PlayerInformation m_PlayerInfo;
     [HideInInspector] public EMovementStates m_CurrentState;
 
-    Vector3 m_desiredDirection;
-    float m_force;
-    float m_drag;
-    bool m_isGrounded;
+    Vector3 m_desiredDirection = new Vector3();
+    float m_force = 0;
+    float m_drag = 0;
+    bool m_isGrounded = true;
     #endregion
 
     /// <summary>
-    /// Uses the charController and performs a Movement with the given Values of the InputMaster
+    /// Changes the velocity of the players rigidbody according to the desired direction and force
     /// </summary>
     public void Move()
+    {
+        //Move Player with velocity of desired direction and force
+        m_PlayerInfo.RB.velocity = m_desiredDirection + Vector3.right * m_force;
+    }
+    /// <summary>
+    /// Calculates the desired Direction with Input, Gravity, Jump and Crouch, also changes the according parameters for the current state
+    /// </summary>
+    public void CalculateDir()
     {
         //InputValues
         m_desiredDirection.x = m_PlayerInfo.Input.m_movement.m * m_PlayerInfo.GP.MovementSpeed;
@@ -35,8 +43,8 @@ public class MovementController : MonoBehaviour
         m_PlayerInfo.Ani.SetFloat("Move", m_PlayerInfo.Input.m_movement.m * m_PlayerInfo.Forward); //SetAnimator Pramater for Movement
 
 
+        //Chrouching
         m_PlayerInfo.Ani.SetBool("Crouch", m_PlayerInfo.Input.m_movement.c); //SetAnimator Pramater for Crouching
-        //Chrouch Movement
         if (m_desiredDirection.x * m_PlayerInfo.Forward < 0)
             if (m_PlayerInfo.Input.m_movement.c)
             {
@@ -44,10 +52,9 @@ public class MovementController : MonoBehaviour
                 m_PlayerInfo.Ani.SetFloat("Move", 0);
             }
 
-        m_PlayerInfo.Ani.SetBool("Jump", false); //SetAnimator Pramater for Jumping
         //Jumping
+        m_PlayerInfo.Ani.SetBool("Jump", false); //SetAnimator Pramater for Jumping
         if (m_isGrounded && !m_PlayerInfo.Input.m_movement.c)
-        {
             if (m_PlayerInfo.Input.m_movement.j)
             {
                 m_PlayerInfo.Ani.SetBool("Jump", true); //SetAnimator Pramater for Jumping
@@ -55,34 +62,37 @@ public class MovementController : MonoBehaviour
                 m_desiredDirection.y = m_PlayerInfo.GP.JumpForce; //Set InputValues for Jumping
                 Force(m_PlayerInfo.GP.JumpDashForce * m_desiredDirection.x, 10); //Jumping Dashing forward/ backward
             }
-        }
 
         //Calculating Gravity
         if (!m_isGrounded)
             m_desiredDirection.y += m_PlayerInfo.GP.GravityForce * Time.deltaTime;
-
-        //Move Player with velocity of desired direction
-        m_PlayerInfo.RB.velocity = m_desiredDirection + Vector3.right * m_force;
     }
     /// <summary>
-    /// When the Contraint is true then perform Fall() instead of Move()
+    /// Sets the default desired Direction and zero force for Move to calculate only the custom gravity
     /// </summary>
-    public void Fall()
+    public void DefaultDir()
     {
         //Reset Values
-        m_force = 0;
         m_desiredDirection.x = 0;
 
         //Calculating Gravity
         if (!m_isGrounded)
             m_desiredDirection.y += m_PlayerInfo.GP.GravityForce * Time.deltaTime;
-
-        m_PlayerInfo.RB.velocity = m_desiredDirection + Vector3.right * m_force;
     }
 
     #region //Utilities
     /// <summary>
-    /// Changes the Flag Enum representing the state the player is currently in
+    /// Resets the Values of the Players Height and Animation
+    /// </summary>
+    public void ResetValues()
+    {
+        m_PlayerInfo.RB.gameObject.transform.position = new Vector3(m_PlayerInfo.IsLeft ? -m_PlayerInfo.GP.PlayerStartPos : m_PlayerInfo.GP.PlayerStartPos, 0, 0);
+
+        m_PlayerInfo.Ani.SetFloat("Move", 0);
+        m_PlayerInfo.Ani.SetBool("Crouch", false);
+    }
+    /// <summary>
+    /// Changes the Flag Enum representing the state the of player
     /// </summary>
     public void SetState()
     {
@@ -101,8 +111,8 @@ public class MovementController : MonoBehaviour
     }
     public void SetHeight()
     {
-        //Crouching
-        m_PlayerInfo.Collider.size = new Vector3(m_PlayerInfo.GP.PlayerRadius, m_PlayerInfo.GP.PlayerHeight, 1);
+        this.m_PlayerInfo.Col.size = new Vector3(m_PlayerInfo.GP.PlayerRadius, m_PlayerInfo.GP.PlayerHeight, 1);
+        this.m_PlayerInfo.Col.center = new Vector3(0, m_PlayerInfo.GP.PlayerHeight * 0.5f, 0);
     }
     public void Force(float _dir, float _drag = 30)
     {
@@ -112,12 +122,15 @@ public class MovementController : MonoBehaviour
     public void Drag()
     {
         if (m_force > 0)
-            m_force -= m_drag * Time.deltaTime;
+        {
+            m_force -= m_drag * Time.unscaledDeltaTime;
+            m_force = Mathf.Clamp(m_force, 0, m_force);
+        }
         if (m_force < 0)
-            m_force += m_drag * Time.deltaTime;
-
-        if (Mathf.Abs(m_force) < 0.31f)
-            m_force = 0;
+        {
+            m_force += m_drag * Time.unscaledDeltaTime;
+            m_force = Mathf.Clamp(m_force, m_force, 0);
+        }
     }
     void OnCollisionEnter(Collision _collision)
     {
@@ -136,17 +149,6 @@ public class MovementController : MonoBehaviour
             m_CurrentState |= _currentState;
         else
             m_CurrentState &= ~_currentState;
-    }
-    /// <summary>
-    /// Resets the Values of the Players Height and Animation
-    /// </summary>
-    /// <param name="_leftSide">to place the palyer in the correct starting position</param>
-    public void ResetValues()
-    {
-        m_PlayerInfo.RB.gameObject.transform.position = new Vector3(m_PlayerInfo.IsLeft ? -m_PlayerInfo.GP.PlayerStartPos : m_PlayerInfo.GP.PlayerStartPos, 0, 0);
-
-        m_PlayerInfo.Ani.SetFloat("Move", 0);
-        m_PlayerInfo.Ani.SetBool("Crouch", false);
     }
     #endregion
 }
