f259f3f2e2e86ca16adf774aa115da323575f029
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-07 19:14:52 +0100

    Added Scriptable Object for Attack State Information, holding values like Aniamtion Clip, AttackState, DamgeType, Damage Amount, Range, Dash, Freezetime, Activation/ Damage/ Recovery FramTime;

---
+++ Street Arena Encounter/Assets/Scripts/Scriptables/Scripts/Attack_Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct SFrameBasedAtackSettings
+{
+    public EAttackStates State;
+    public AnimationClip AnimationClip;
+    public EDamageStates DamageType;
+    public float Damage_Amount;
+    public float Damage_Range;
+    public bool Dash;
+    public bool FreezeTime;
+    public float Activation_FrameTime;
+    public float Damage_FrameTime;
+    public float Recovery_FrameTime;
+}
+
+[CreateAssetMenu(menuName = "Settings/Attack Settings", fileName = "Attack Settings", order = 2)]
+public class Attack_Settings : ScriptableObject
+{
+    public SFrameBasedAtackSettings[] Attacks;
+}
