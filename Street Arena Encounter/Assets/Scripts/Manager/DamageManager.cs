072d6d0060de5373aa37c8a5dc89c5af99d71f66
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-07 19:27:09 +0100

    Cleaned up DamageManager code, now more readable; Damage Amount now passed on from DealDamage as a parameter and Updated PerformDamage IEnumerator to be cleaner; Decommented CompareStates method now in calculation;

--- Street Arena Encounter/Assets/Scripts/Manager/DamageManager.cs
+++ Street Arena Encounter/Assets/Scripts/Manager/DamageManager.cs
@@ -9,8 +9,6 @@ public class DamageManager : MonoBehaviour
     [SerializeField] ParticleSystem[] m_ps_L = new ParticleSystem[3];
     [SerializeField] ParticleSystem[] m_ps_R = new ParticleSystem[3];
 
-    bool m_toLeftSide;
-
     void Awake()
     {
         if (Instance)
@@ -20,85 +18,39 @@ public class DamageManager : MonoBehaviour
         }
         Instance = this;
     }
-    public bool DealDamage(float _amount, bool _toLeftSide, EDamageStates _damageType)
+    public bool DealDamage(bool _toLeftSide, float _amount, EDamageStates _damageType)
     {
-        m_toLeftSide = _toLeftSide;
+        PlayerInformation target = _toLeftSide ?
+                    GameManager.Instance.m_Player_L :
+                    GameManager.Instance.m_Player_R;
 
-        if (EvaluateDamage())
+        if (!GameManager.Instance.BoolDistance(2) && CompareStates(target))
         {
-            StartCoroutine(PerformDamage(_amount, _damageType));
+            StartCoroutine(PerformDamage(
+                target,
+                _amount,
+                _damageType));
             return true;
         }
 
         return false;
     }
 
-    bool EvaluateDamage()
+    bool CompareStates(PlayerInformation _playerInfo)
     {
-        if (!GameManager.Instance.BoolDistance(2))// && CompareStates())
-            return true;
-        return false;
-    }
+        EAttackStates currentState_Attack = _playerInfo.Player.m_AttackController.m_CurrentState;
+        EMovementStates currentState_Movement = _playerInfo.Player.m_MovementController.m_CurrentState;
 
-    bool CompareStates()
-    {
-        if (m_toLeftSide)
-        {
-            if (GameManager.Instance.m_Player_L.Player.m_AttackController.m_CurrentState == EAttackStates.Block
-                || GameManager.Instance.m_Player_L.Player.m_MovementController.m_CurrentState == EMovementStates.MoveBackwards)
-                return false;
-        }
-        else
-        {
-            if (GameManager.Instance.m_Player_R.Player.m_AttackController.m_CurrentState == EAttackStates.Block
-                || GameManager.Instance.m_Player_R.Player.m_MovementController.m_CurrentState == EMovementStates.MoveBackwards)
-                return false;
-        }
+        if (currentState_Attack == EAttackStates.Block
+            || currentState_Movement == EMovementStates.MoveBackwards)
+            return false;
 
         return true;
     }
 
-    IEnumerator PerformDamage(float _damageAmount, EDamageStates _damageType)
+    public void FallBack(PlayerInformation _playerInfo, float _force = 5, float _drag = 20)
     {
-        if (m_toLeftSide)
-        {
-            if (GameManager.Instance.m_Init.m_GameMode != EGameModes.TRAINING)
-                GameManager.Instance.m_Player_L.Health -= _damageAmount;
-            GameManager.Instance.m_Player_L.Ani.SetTrigger("Damaged");
-
-            if (GameManager.Instance.m_Player_L.Health <= 20 && GameManager.Instance.m_Player_L.Special)
-            {
-                AttackManager.Instance.SetSpecial(m_toLeftSide);
-                GameManager.Instance.m_Player_L.Special = false;
-            }
-
-            m_ps_L[(int)_damageType].Play();
-        }
-        else
-        {
-            if (GameManager.Instance.m_Init.m_GameMode != EGameModes.TRAINING)
-                GameManager.Instance.m_Player_R.Health -= _damageAmount;
-            GameManager.Instance.m_Player_R.Ani.SetTrigger("Damaged");
-            
-            if (GameManager.Instance.m_Player_R.Health <= 20 && GameManager.Instance.m_Player_R.Special)
-            {
-                AttackManager.Instance.SetSpecial(m_toLeftSide);
-                GameManager.Instance.m_Player_R.Special = false;
-            }
-
-            m_ps_R[(int)_damageType].Play();
-        }
-
-        FallBack(GameManager.Instance.m_Player_R, 5);
-
-        StartCoroutine(Shake(25, 0.2f));
-
-        yield return null;
-    }
-
-    public void FallBack(PlayerInformation _playerInfo, float _force)
-    {
-        _playerInfo.Player.m_MovementController.Force(_force * -_playerInfo.Forward);
+        _playerInfo.Player.m_MovementController.Force(_force * -_playerInfo.Forward, _drag);
     }
 
     IEnumerator Shake(float _intensity, float _duration)
@@ -109,6 +61,36 @@ public class DamageManager : MonoBehaviour
 
         yield return new WaitForSeconds(_duration);
         noise.m_FrequencyGain = 1;
+
+        yield return null;
+    }
+    IEnumerator PerformDamage(PlayerInformation _playerInfo, float _damageAmount, EDamageStates _damageType)
+    {
+        //Damage
+        if (GameManager.Instance.m_Init.m_GameMode != EGameModes.TRAINING)
+            _playerInfo.Health -= _damageAmount; //substracs health with damag amount
+
+        _playerInfo.Ani.SetTrigger("Damaged"); //plays damage animation
+
+
+        //Sepcial
+        if (_playerInfo.Health <= 20 && _playerInfo.Special)
+        {
+            AttackManager.Instance.SetSpecial(_playerInfo.IsLeft, true);
+            _playerInfo.Special = false;
+        }
+
+        //ParticleSystem
+        int i = Mathf.Abs((int)_damageType - 1); //the index of the particleSystem-Array
+        if (_playerInfo.IsLeft)
+            m_ps_L[i].Play(); //plays the damage particleSystem
+        else
+            m_ps_R[i].Play(); //plays the damage particleSystem
+
+        //Force
+        FallBack(_playerInfo); //Forces the player back
+        //Shake
+        StartCoroutine(Shake(25, 0.2f)); //Shakes the vmcamera
 
         yield return null;
     }
