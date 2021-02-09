eb425d622765a6d25d65c6e37879fd902bdd18cd
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-09 21:54:23 +0100

    Added InputManager, has playerinput of players and gets it to the main scene from menu and in endScreen holds it for revenge and when in main gets destroyed because of the anti-singleton; Has Method to create icon as child of parent;

---
+++ Street Arena Encounter/Assets/InputManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum EPIIconType
+{
+    KEYBOARD,
+    GAMEPAD
+}
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    public PlayerInput m_Player_L_Input;
+    public PlayerInput m_Player_R_Input;
+    [SerializeField] GameObject m_icon_pi_controller;
+    [SerializeField] GameObject m_icon_pi_keyboard;
+
+
+    void Awake()
+    {
+        if (Instance)
+            Destroy(Instance.gameObject);
+        Instance = this;
+    }
+    public void OnDestroy()
+    {
+        Destroy(m_Player_L_Input);
+        Destroy(m_Player_R_Input);
+    }
+
+    public void CreateIcon(EPIIconType _piIconType, Transform _parent)
+    {
+        switch (_piIconType)
+        {
+            case EPIIconType.KEYBOARD:
+                Instantiate(m_icon_pi_keyboard, _parent);
+                break;
+            case EPIIconType.GAMEPAD:
+                Instantiate(m_icon_pi_controller, _parent);
+                break;
+            default:
+                break;
+        }
+    }
+}
