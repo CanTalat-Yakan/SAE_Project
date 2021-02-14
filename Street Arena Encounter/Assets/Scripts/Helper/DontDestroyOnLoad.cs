7a4399c11d429f0d917770083a411dc25d9c078b
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-14 01:18:02 +0100

    Updated DontDestroyOnLoad with Singleton function with bool enabling it or not;

--- Street Arena Encounter/Assets/Scripts/Helper/DontDestroyOnLoad.cs
+++ Street Arena Encounter/Assets/Scripts/Helper/DontDestroyOnLoad.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    static DontDestroyOnLoad Instance;
+    [SerializeField] bool m_Singleton;
+
+    void Awake()
+    {
+        if (Instance)
+            if (m_Singleton)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        Instance = this;
+    }
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
