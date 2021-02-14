47391c3da343645bcc990140d1c1dba56da36623
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-14 03:47:00 +0100

    Created HelperClass for FighterSelection in the Menu-Scene;

---
+++ Street Arena Encounter/Assets/Scripts/Helper/FighterSelection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterSelection : MonoBehaviour
+{
+    [SerializeField] Main_Init m_init;
+    [SerializeField] List<Fighter_Info> m_fighterInfos = new List<Fighter_Info>();
+
+    public void SetFighterInfo_L(Fighter_Info _Player)
+    {
+        m_init.m_Player_L = _Player;
+    }
+
+    public void SetFighterInfo_R(Fighter_Info _Player)
+    {
+        m_init.m_Player_R = _Player;
+        InfoBoxManager.Instance.UpdateList();
+    }
+    public void SetRandomFighters()
+    {
+        SetFighterInfo_L(
+            m_fighterInfos[
+                Random.Range(
+                    0, 
+                    m_fighterInfos.Count)]);
+        SetFighterInfo_R(
+            m_fighterInfos[
+                Random.Range(
+                    0,
+                    m_fighterInfos.Count)]);
+    }
+}
