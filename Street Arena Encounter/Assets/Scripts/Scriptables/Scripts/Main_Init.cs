593bf7400ea28ca9cd8552c119b0307c23af52bc
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-14 03:47:53 +0100

    Updated and Improved MainInit to hold  Fighter-Infos and added randomLevel method;

--- Street Arena Encounter/Assets/Scripts/Scriptables/Scripts/Main_Init.cs
+++ Street Arena Encounter/Assets/Scripts/Scriptables/Scripts/Main_Init.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-[Serializable]
-public struct SInitInfo
-{
-    public string Name;
-}
 
 [CreateAssetMenu(menuName = "Init/Main Init", fileName = "Main Init", order = 0)]
 public class Main_Init : ScriptableObject
 {
     public int GameMode { set => m_GameMode = (EGameModes)value; }
+    [Header("Level Attributes")]
     public EGameModes m_GameMode;
     public int Level { set => m_Level = (ELevels)value; }
     public ELevels m_Level;
@@ -21,11 +15,20 @@ public class Main_Init : ScriptableObject
     public int m_Rounds;
     public int m_Timer;
 
+    [Header("Main Attributes")]
     public int m_LoadingScreenTime;
     public bool m_SkipIntro;
 
-    public SInitInfo m_Player_L;
-    public SInitInfo m_Player_R;
+
+    [Header("Fighter Attributes")]
+    public Fighter_Info m_Player_L;
+    public Fighter_Info m_Player_R;
+
+
+    public void RandomLevel()
+    {
+        m_Level = (ELevels)Random.Range(0, sizeof(ELevels));
+    }
 }
 public enum EGameModes
 {
