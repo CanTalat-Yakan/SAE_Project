b7ce7634ace759b43d651456ddde63d808b034fe
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-14 03:45:47 +0100

    Created ScriptableObject for FighterInformation, Name, Splash-Image, Model and AnimatorOverrideController;

---
+++ Street Arena Encounter/Assets/Scripts/Scriptables/Scripts/Fighter_Info.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Char/Fighter Info", fileName = "Fighter Init", order = 0)]
+public class Fighter_Info : ScriptableObject
+{
+    public string Name;
+    public Image Image;
+    public GameObject Model;
+    public AnimatorOverrideController AnimatorOC;
+}
