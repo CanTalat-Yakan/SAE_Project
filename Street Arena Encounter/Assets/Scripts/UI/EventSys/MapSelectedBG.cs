c213263f3c3cda439c410abf4e7e641f45af197a
CanTalat-Yakan <can.talat.yakan@gmail.com>
2021-02-25 00:23:41 +0100

    Updated and Improved Design in UI;

--- Street Arena Encounter/Assets/Scripts/UI/EventSys/MapSelectedBG.cs
+++ Street Arena Encounter/Assets/Scripts/UI/EventSys/MapSelectedBG.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class MapSelectedBG : MonoBehaviour, EventInterface
 {
     [SerializeField] Image m_bg;
+    [SerializeField] Image m_lobby_bg;
     [SerializeField] Sprite m_img;
 
     public void Action()
     {
         m_bg.sprite = m_img;
+        m_lobby_bg.sprite = m_img;
     }
 }
